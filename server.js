8cad8f1f7e6a1ce00c79ebd23220113351f760da
vpolier <111066480+vpolier@users.noreply.github.com>
2023-05-11 15:55:39 +1000

    update

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const path = require('path');
 const webRouter = require('./routes/api');
 const PORT = process.env.PORT || 3001;
+const _dirname = "";
 
 // web routs serve htmls
 
 const app = express();
-
-app.use(webRouter);
-// api routs serve json
-
 
 // Hiddleware for pasing JSON and urlencoded from data 
 app.use(express.json());
@@ -17,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // tell the server look in public folder for extra files the cliente needs
 app.use(express.static('public'));
-app.use("/api/notes", notesRouter) ;
+app.use("/api/notes", webRouter) ;
 
 // GET Route for home page
 app.get('/*', (req, res) =>
@@ -26,7 +23,7 @@ app.get('/*', (req, res) =>
 
 //to send back the note.html
 app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(_dirname, '/public/notes.html'))
 );
 
 app.listen(PORT,()=> {
